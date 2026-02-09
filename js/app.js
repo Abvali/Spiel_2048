6c6c3ba10ea9d95c7b9aa15e465b79ce2631a995
Maryam <abvali.maryam@gmail.com>
2026-02-09 17:53:45 +0100

    move-function

--- js/app.js
+++ js/app.js
@@ -3,26 +3,13 @@ const group = (css) => document.querySelectorAll(css);
 const create = (html) => document.createElement(html);
 
 let board; //Spielfeld
-
 let rows = 4;
 let columns = 4;
 
-// initial board
-function createBoard() {
-  board = [
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-  ];
-  for (let i = 0; i < rows; i++) {
-    for (let j = 0; j < columns; j++) {
-      const tile = create("div");
-      tile.classList.add("tile");
-      el("#board").append(tile);
-    }
-  }
-}
+// window.addEventListener("DOMContentLoaded", () => {
+//   createBoard();
+//   renderBoard();
+// });
 
 // initial board
 function createBoard() {
@@ -39,6 +26,8 @@ function createBoard() {
       el("#board").append(tile);
     }
   }
+  addNum();
+  addNum();
 }
 
 // Die Zahl 2 oder 4 ermitteln
@@ -67,9 +56,98 @@ function addNum() {
 
   const twoFour = twoOrFour();
   board[placeToPush.r][placeToPush.c] = twoFour;
-  console.log(twoFour);
+}
+
+// Bewegung auf jedem row auf dem Spielfeld
+function slideLeft(row) {
+  let merged = [];
+  let arr = [];
+  for (let i = 0; i < row.length; i++) {
+    if (row[i] !== 0) {
+      arr.push(row[i]);
+    }
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === arr[i + 1]) {
+      merged.push(arr[i] * 2);
+      i++;
+    } else {
+      merged.push(arr[i]);
+    }
+  }
+  while (merged.length < 4) {
+    merged.push(0);
+  }
+  return merged;
+}
+
+// Bewegung nach Links
+function moveLeft() {
+  for (let i = 0; i < rows; i++) {
+    board[i] = slideLeft(board[i]);
+  }
+  addNum();
+  renderBoard();
+}
+
+//Bevegung nach Rechts (reverse -> slideLeft -> reverse)
+function moveRight() {
+  for (let i = 0; i < rows; i++) {
+    const newBoard = slideLeft([...board[i]].reverse());
+    board[i] = newBoard.reverse();
+  }
+  addNum();
+  renderBoard();
+}
+
+//Bevegung nach Oben (transpose -> moveLeft -> transpose)
+function moveUp() {
+  console.log("up");
+}
+
+//Bevegung nach Rechts (transpose -> moveRight -> transpose)
+function moveDown() {
+  console.log("down");
+}
+// Pfeiltaste Event
+document.addEventListener("keydown", (e) => {
+  if (e.code === "ArrowLeft") {
+    moveLeft();
+  } else if (e.code === "ArrowRight") {
+    moveRight();
+  } else if (e.code === "ArrowUp") {
+    moveUp();
+  } else if (e.code === "ArrowDown") {
+    moveDown();
+  }
+});
+
+function renderBoard() {
+  const tile = group(".tile");
+
+  // console.log(tile);
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < columns; j++) {
+      let index = i * columns + j;
+
+      // Alle Classen mit x anfangen entfernen
+      for (let cls of tile[index].classList) {
+        if (cls.startsWith("x")) {
+          tile[index].classList.remove(cls);
+        }
+      }
+      const value = board[i][j];
+      if (value === 0) {
+        tile[index].innerText = "";
+      } else if (value > 0) {
+        tile[index].innerText = value;
+        tile[index].classList.add(`x${value}`);
+      }
+    }
+  }
+  // console.log(tile);
 }
 
 createBoard();
-addNum();
+renderBoard();
 console.dir(board);
